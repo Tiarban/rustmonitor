b8e132b8877665d56edd2b21d8170de5faac0eff
Tiarban <tiarnanryan13@gmail.com>
2025-01-22 23:30:07 +0000

    not functioning yet but added embassy and began async functionality, no more std!

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,9 @@
-use std::{thread::sleep, time::Duration, sync::{Mutex, Arc}, str::from_utf8, collections::HashMap};
+#![no_std] //using embassy instead of std libraries
+#![no_main] //not using fn main, async main instead
+#![feature(type_alias_impl_trait)] //simplifies fn signatures for async
+
+//use std::{thread::sleep, time::Duration, sync::{Mutex, Arc}, str::from_utf8, collections::HashMap};
+//using embassy time now instead of std
 
 use anyhow::{self, Error};
 use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
@@ -6,18 +11,37 @@ use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::wifi::EspWifi;
-use heapless::String; //for correct ssid data conversion
+use heapless::String; //for correct ssid & password data conversion
 
-const SSID: &str = "";
-const PASS: &str = ""; //should be hardcoded for now
+use embassy_executor::Spawner; //embassy's executor's spawner
+use embassy_time::{Duration, Timer}; //now duration and timer is from embassy time
+use esp_backtrace as _; //as _ imported but not used
+use esp_hal::{
+    clock::ClockControl,
+    embassy::{self},
+    peripherals::Peripherals,
+    prelude::*,
+    timer::TimerGroup,
+}; //using esp-hal for this stuff rather than std
 
-fn main() -> anyhow::Result<()> { //anyhow::result is return type leveraging error control
+#[embassy_executor::task]
+async fn read_current () { //for reading data
+    loop{
+        esp_println!("Reading data") //placeholder
+        Time::after(Duration::from_millis(1000)).await; //return ctrl to executor
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) -> anyhow::Result<()> { //anyhow::result is return type leveraging error control
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
 
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    info!("Hello, world!");
 
     //handles:
 
@@ -36,7 +60,7 @@ fn main() -> anyhow::Result<()> { //anyhow::result is return type leveraging err
     //wifi mut is now the wifi handle driver (like peripherals)
 
     let mut ssid = String::<32>::new();
-    ssid.push_str("Three_E42796").unwrap();
+    ssid.push_str("Three_E42796").unwrap(); //explicit type required for config for both 
 
     let mut password = String::<64>::new();
     password.push_str("2hG{w?24").unwrap(); 
@@ -60,22 +84,26 @@ fn main() -> anyhow::Result<()> { //anyhow::result is return type leveraging err
     
     wifi.set_configuration(&Configuration::Client(ClientConfiguration { //configuration for client
         ssid,
-        password, //explicit conversion -- .into() type conversion fails
+        password, //explicit defining done above - examples for using .into() or from here dont work
         auth_method: AuthMethod::None, //could be used later for WPA3 implementation
         ..Default::default() //fills non specified methods with default config
     }))?; //note these brackets are closed here for the nested struture of accessing the clientconfig 
 
-    wifi.start()?;
+    wifi.start().await?;
 
-    wifi.connect()?; //self explanatory lines
+    wifi.connect().await?; //self explanatory lines
 
     while !wifi.is_connected().unwrap() {
         // Get and print connetion configuration
         let config = wifi.get_configuration().unwrap();
-        println!("Waiting for station {:?}", config);
+        esp_println!("Waiting for station {:?}", config);
+
+        Timer::after(Duration::from_millis(5000)).await; //added non blocking delay before checking again if wifi is connected 
     }
     
-    println!("Connected"); //boilerplate for checking connection
+    esp_println!("Connected"); //boilerplate for checking connection
+
+    spawner.spawn(read_current()).ok();
 
     Ok(()) //returns Result type if it's ok - no errors detected
 }
