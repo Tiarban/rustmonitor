83861583e16314a9f5bed0ddd48b4b03fa6c5a29
Tiarban <tiarnanryan13@gmail.com>
2025-01-22 14:19:26 +0000

    client wifi template code added

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,16 @@
+use std::{thread::sleep, time::Duration, sync::{Mutex, Arc}, str::from_utf8, collections::HashMap};
+
+use accel_stepper::{Driver, OperatingSystemClock};
+use embedded_svc::{wifi::{Configuration, ClientConfiguration, AuthMethod}, http::Method::Post,http::Method::Get, io::Read};
+use esp_idf_hal::{peripheral::Peripheral, prelude::Peripherals, gpio::PinDriver, ledc::{LedcTimerDriver, config::TimerConfig, LedcDriver}};
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::{EspNvsPartition, NvsDefault, EspDefaultNvsPartition}, timer::{EspTimerService, Task, EspTaskTimerService}, wifi::{AsyncWifi, EspWifi}, ping::EspPing, http::server::EspHttpServer};
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported (i am)
+use log::*;
+use esp_idf_hal::units::*;
+
+const SSID: &str = "";
+const PASS: &str = ""; //should be hardcoded for now
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -6,5 +19,60 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    let peripherals = Peripherals::take().unwrap();
+    let sysloop = EspSystemEventLoop::take().unwrap();
+    let timer_service = EspTaskTimerService::new().unwrap();
+    let _wifi = wifi(peripherals.modem, sysloop,Some(EspDefaultNvsPartition::take().unwrap()),timer_service).unwrap(); //nvs saves wifi state to persistent memory
+
     log::info!("Hello, world!");
 }
+
+//wifi connect code
+
+pub fn wifi(
+    modem: impl Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
+    sysloop: EspSystemEventLoop,
+    nvs: Option<EspNvsPartition<NvsDefault>>,
+    timer_service: EspTimerService<Task>,
+) -> anyhow::Result<AsyncWifi<EspWifi<'static>>> {
+    use futures::executor::block_on;
+    let mut wifi = AsyncWifi::wrap(
+        EspWifi::new(modem, sysloop.clone(), nvs)?, sysloop,
+        timer_service.clone(),
+    )?;
+
+    block_on(connect_wifi(&mut wifi))?;
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+
+    println!("Wifi DHCP info: {:?}", ip_info);
+    
+    EspPing::default().ping(ip_info.subnet.gateway, &embedded_svc::ping::Configuration::default())?;
+    Ok(wifi)
+
+}
+
+async fn connect_wifi(wifi: &mut AsyncWifi<EspWifi<'static>>) -> anyhow::Result<()> {  //non blocking async function
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.into(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: PASS.into(),
+        channel: None,
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start().await?;
+    info!("Wifi started"); //? propogates error automatically, only returning success if Ok type produced from Result enum
+
+    wifi.connect().await?;
+    info!("Wifi connected"); //await 
+
+    wifi.wait_netif_up().await?;
+    info!("Wifi netif up");
+
+    Ok(())   //if no error detected
+}
+
+//end of wifi connect code
